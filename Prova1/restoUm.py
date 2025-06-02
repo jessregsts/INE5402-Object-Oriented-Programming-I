acc5417168234e592039c3060e6ea13a1fde4c19
jessregsts <jessicars241@gmail.com>
2025-06-01 21:44:56 -0300

    Create restoUm.py

---
+++ Prova1/restoUm.py
@@ -0,0 +1,6 @@
+ # para ajudar john!
+ A = int(input())
+ B = int(input())
+ # primeiro passo   '%' = resto na linguagem python
+ # segundo passo     logo, é simples criar um programa ao amigo
+ print(A % B)
